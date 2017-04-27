8113446bad3bf21c35450c06870ace5dd95bea51
Matěj Týč <matej.tyc@gmail.com>
2017-04-27 12:18:43 +0200

    Added basic stub of database and GET requests processing

--- src/iwant_bot/start.py
+++ src/iwant_bot/start.py
@@ -1,12 +1,28 @@
 from aiohttp import web
 
+import db
+
+
+DB_ACCESS = None
+
+def format_message(message):
+   ret = "'{0.text}' by '{0.nickname}' at {0.when}".format(message)
+   return ret
+
+
 async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+    name = request.rel_url.query.get('name', "anonymous")
+    message = request.rel_url.query.get('msg', "")
+    DB_ACCESS.save_message(message, name)
 
-app = web.Application()
-app.router.add_get('/', handle)
-app.router.add_get('/{name}', handle)
+    messages_we_got_so_far = DB_ACCESS.load_n_last_messages()
+    text = ["We got these entries so far:\n"]
+    text.extend([format_message(msg) for msg in messages_we_got_so_far])
+    return web.Response(text="\n".join(text))
 
-web.run_app(app)
+
+if __name__ == "__main__":
+    DB_ACCESS = db.DatabaseAccess()
+    app = web.Application()
+    app.router.add_get('/', handle)
+    web.run_app(app)
