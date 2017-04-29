cae0e58b725f361c7c1a5ab94923e4a3f0c1ca75
Matěj Týč <matej.tyc@gmail.com>
2017-04-29 22:43:21 +0200

    Modified the project so it is gunicorn-compatible.
    Also updated README.md accordingly.

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Slackbot project implemented by [Kiwi.com](Kiwi.com) interns
 ## WIP
 - first internship crew started on this 21th April 2017
 - MVP is going to be available at the end of May 2017
+
+## How to set up
+
+* Make sure you have `pip-tools`, `docker` and `docker-compose`.
+* Run `docker-compose up`.
+* If this step shows signs of failiure, regenerate `requirements.txt` from `requirements.in` and then run `docker-compose build` followed by `docker-compose up`.
+* Visit `http://localhost:8080` in your browser. Read the instructions and check out some GET requests!.
--- src/iwant_bot/start.py
+++ src/iwant_bot/start.py
@@ -5,8 +5,9 @@ from iwant_bot import db
 
 DB_ACCESS = None
 
+
 def format_message(message):
-   ret = "'{0.text}' by '{0.nickname}' at {0.when}".format(message)
+   ret = f"'{message.text}' by '{message.nickname}' at {message.when}"
    return ret
 
 
@@ -16,13 +17,16 @@ async def handle(request):
     DB_ACCESS.save_message(message, name)
 
     messages_we_got_so_far = DB_ACCESS.load_n_last_messages()
-    text = ["We got these entries so far:\n"]
+    text = ["We recognize the 'name' and 'msg' GET query attributes,"]
+    text += ["so you can do http://localhost:8080/?name=me&msg=message"]
+    text += ["We got these entries so far:\n"]
     text.extend([format_message(msg) for msg in messages_we_got_so_far])
     return web.Response(text="\n".join(text))
 
 
+app = web.Application()
+app.router.add_get('/', handle)
+DB_ACCESS = db.DatabaseAccess()
+
 if __name__ == "__main__":
-    DB_ACCESS = db.DatabaseAccess()
-    app = web.Application()
-    app.router.add_get('/', handle)
     web.run_app(app)
