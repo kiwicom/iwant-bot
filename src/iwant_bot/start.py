9c7114791948276cec7f671281b7c723e7b3975d
Matěj Týč <matej.tyc@gmail.com>
2017-05-03 12:45:48 +0200

    Changed quotes from " to ' in the Python app.

--- src/iwant_bot/start.py
+++ src/iwant_bot/start.py
@@ -12,21 +12,21 @@ def format_message(message):
 
 
 async def handle(request):
-    name = request.rel_url.query.get('name', "anonymous")
-    message = request.rel_url.query.get('msg', "")
+    name = request.rel_url.query.get('name', 'anonymous')
+    message = request.rel_url.query.get('msg', '')
     DB_ACCESS.save_message(message, name)
 
     messages_we_got_so_far = DB_ACCESS.load_n_last_messages()
     text = ["We recognize the 'name' and 'msg' GET query attributes,"]
-    text += ["so you can do http://localhost:8080/?name=me&msg=message"]
-    text += ["We got these entries so far:\n"]
+    text += ['so you can do http://localhost:8080/?name=me&msg=message']
+    text += ['We got these entries so far:\n']
     text.extend([format_message(msg) for msg in messages_we_got_so_far])
-    return web.Response(text="\n".join(text))
+    return web.Response(text='\n'.join(text))
 
 
 app = web.Application()
 app.router.add_get('/', handle)
 DB_ACCESS = db.DatabaseAccess()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     web.run_app(app)
