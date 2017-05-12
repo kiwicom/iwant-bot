0e7dbd70c496cb08fecdd5280de8e1d57da0e8ce
xmalmed <xmad1@volna.cz>
2017-05-12 10:44:13 +0200

    Add slack webhook (#20)
    * Add information about Slack WebHooks into README.md
    
    Gunicorn server responses to http POST methods.
    
    Connected to issues #5 and #9
    
    * :memo: Enhance description of Slack WebHooks
    
    * add SQLite files to .gitignore
    
    [ci skip]

--- src/iwant_bot/start.py
+++ src/iwant_bot/start.py
@@ -24,12 +24,18 @@ async def handle(request):
     text = ["We recognize the 'name' and 'msg' GET query attributes,"]
     text += ['so you can do http://localhost:8080/?name=me&msg=message']
     text += ['We got these entries so far:\n']
+    text += ['Or you can test POST request by command:']
+    text += ['> curl -X POST some_url_like_https://kiwislackbot1.localtunnel.me/\n']
     text.extend([format_message(msg) for msg in messages_we_got_so_far])
     return web.Response(text='\n'.join(text))
+
+async def handle_post(request):
+    return web.Response(text=request.method + ' was obtained.\n')
 
 
 app = web.Application()
 app.router.add_get('/', handle)
+app.router.add_post('/', handle_post)
 DB_ACCESS = db.DatabaseAccess()
 
 if __name__ == '__main__':
