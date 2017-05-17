da302f78cdb9a3140b19b0782bcf033a99b15557
xmalmed <xmalmed@gmail.com>
2017-05-17 23:12:23 +0200

    Improve the handler of Slack POST requests.
    Add POST handler to src/start.py.
    Change Dockerfile to obtain realtime log from gunicorn.
    Update README.md about configuration of Slack WebHooks.
    Close issue #22

--- src/iwant_bot/start.py
+++ src/iwant_bot/start.py
@@ -25,12 +25,65 @@ async def handle(request):
     text += ['so you can do http://localhost:8080/?name=me&msg=message']
     text += ['We got these entries so far:\n']
     text += ['Or you can test POST request by command:']
-    text += ['> curl -X POST some_url_like_https://kiwislackbot1.localtunnel.me/\n']
+    text += ['> curl -X POST  --data "user_name=0&channel_id=1&channel_name=2&service_id=3&']
+    text += ['  team_domain=4&team_id=5&text=6&timestamp=7&token=9&user_id=9" http://localhost:8080']
+             #curl -X POST some_url_like_https://kiwislackbot1.localtunnel.me/\n']
     text.extend([format_message(msg) for msg in messages_we_got_so_far])
     return web.Response(text='\n'.join(text))
 
+def format_response(message="No response."):
+    ret = f'{{\n\t"text":"{message}"\n}}\n'
+    return ret
+
+def check_post_request(body):
+    '''The structure of POST is given by Slack.
+    https://api.slack.com/custom-integrations/outgoing-webhooks
+    However, "trigger_word" is opional.'''
+
+    expected_fields = {"token", "team_id", "team_domain", "service_id", "channel_id",
+                       "channel_name", "timestamp", "user_id", "user_name", "text"}
+    obtined_fields = set(body.keys())
+
+    if (expected_fields - obtined_fields):
+        print('POST missing these fields: ' + ', '.join(expected_fields - obtined_fields))
+        return False
+
+    return True
+
+
 async def handle_post(request):
-    return web.Response(text=request.method + ' was obtained.\n')
+    body = await request.post()
+    print(request.method)
+    # test of POST body structure
+    if check_post_request(body):
+        token =         body["token"]
+        team_id =       body["team_id"]
+        team_domain =   body["team_domain"]
+        service_id =    body["service_id"]
+        channel_id =    body["channel_id"]
+        channel_name =  body["channel_name"]
+        timestamp =     body["timestamp"]
+        user_id =       body["user_id"]
+        user_name =     body["user_name"]
+        text =          body["text"]
+        try:
+            trigger_word = body["trigger_word"]
+        except KeyError:
+            print("No trigger_word was used.")
+            trigger_word = None
+    else:
+        return web.Response(text='No valid Slack POST request.\n')
+
+    if trigger_word == 'repeat':
+        res = web.json_response(body=format_response(user_name + ' wants me to ' + text))
+        print(res.headers)
+        print(res.body)  # how to display full response body? It is <aiohttp.payload.StringPayload object at 0x7fc9e266c2e8>
+        return res
+        #return web.json_response(body=format_response(user_name + ' wants me to ' + text))
+    elif trigger_word == 'whoami':
+        return web.json_response(body=format_response('You are ' + user_name + ' with id ' + user_id))
+    else:
+        return web.json_response(body=format_response(user_name + ' wrote ' + text))
 
 
 app = web.Application()
