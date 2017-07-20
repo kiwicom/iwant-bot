59429a76a6d224fde5bbca2a4486b5e25030ff1f
Rastislav Kanda <rastislav.kanda@me.com>
2017-07-20 09:59:12 +0200

    Fix code to comply to PEP8

--- src/bot_worker/celery.py
+++ src/bot_worker/celery.py
@@ -4,15 +4,15 @@ from celery import Celery
 worker = Celery('iwant_bot_celery_worker',
                 broker='redis://redis:6379/0',
                 backend='redis://redis:6379/0'
-               )
+                )
 
 worker.conf.update(
-                    enable_utc=True,
-                    broker_use_ssl=False,
-                    timezone='Europe/Prague',
-                    worker_concurrency=6,
-                    imports=['bot_worker.tasks', 'iwant_bot.pool'],
-                  )
+    enable_utc=True,
+    broker_use_ssl=False,
+    worker_concurrency=6,
+    timezone='Europe/Prague',
+    imports=['bot_worker.tasks', 'iwant_bot.pool']
+)
 worker.conf.beat_schedule = {
     # 'check_every_30_seconds': {
     #    'task': 'iwant_bot.pool.make_groups',
