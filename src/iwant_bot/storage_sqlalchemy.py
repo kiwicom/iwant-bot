e2e83653b319e42153c37fa911f11d03d4939952
Matěj Týč <matej.tyc@gmail.com>
2017-07-20 22:00:50 +0200

    Remove unused import.

--- src/iwant_bot/storage_sqlalchemy.py
+++ src/iwant_bot/storage_sqlalchemy.py
@@ -1,7 +1,6 @@
 import abc
 from contextlib import contextmanager
 
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, ForeignKey, Float, Integer
 
