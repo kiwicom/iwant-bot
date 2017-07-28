7d0838060947e141802f4b1116d4871c3a3eafca
Matěj Týč <matej.tyc@gmail.com>
2017-07-28 11:09:59 +0200

    Fix pipeline creation.

--- src/iwant_bot/pipeline.py
+++ src/iwant_bot/pipeline.py
@@ -22,6 +22,5 @@ def choose_correct_store():
     return store
 
 
-pipeline.add_block(request_acquisition.UIDAssigner())
-pipeline.add_block(
-    request_acquisition.Saver(choose_correct_store()))
+pipeline.add_block("activity", request_acquisition.UIDAssigner())
+pipeline.add_block("activity", request_acquisition.Saver(choose_correct_store()))
